GoogleCloudPlatform/gcping

commit 599bd5bb3f6173c9333fc356c7a173273e00b5e4

Report median latency, not average (#13)
* report median latency, not average

* only calculate median once per region

* fix merge error

main.go
@@ -114,23 +114,24 @@ func report() {
 		a := m[o.region]
 
 		a.region = o.region
-		a.duration += o.duration
+		a.durations = append(a.durations, o.durations[0])
 		a.errors += o.errors
 
 		m[o.region] = a
 	}
-	averages := make([]output, 0, len(m))
+	all := make([]output, 0, len(m))
 	for _, t := range m {
-		t.duration = t.duration / time.Duration(number)
-		averages = append(averages, t)
+		all = append(all, t)
 	}
 
-	sorter := &outputSorter{averages: averages}
-	sort.Sort(sorter)
+	// sort all by median duration.
+	sort.Slice(all, func(i, j int) bool {
+		return all[i].median() < all[j].median()
+	})
 
 	tr := tabwriter.NewWriter(os.Stdout, 3, 2, 2, ' ', 0)
-	for i, a := range averages {
-		fmt.Fprintf(tr, "%2d.\t[%v]\t%v", i+1, a.region, a.duration)
+	for i, a := range all {
+		fmt.Fprintf(tr, "%2d.\t[%v]\t%v", i+1, a.region, a.median())
 		if a.errors > 0 {
 			fmt.Fprintf(tr, "\t(%d errors)", a.errors)
 		}

inputoutput.go
@@ -17,6 +17,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sort"
 	"time"
 )
 
@@ -49,8 +50,8 @@ func (i *input) benchmark(fn func() error) {
 	duration := time.Since(start)
 
 	o := output{
-		region:   i.region,
-		duration: duration,
+		region:    i.region,
+		durations: []time.Duration{duration},
 	}
 	if err != nil {
 		o.errors++
@@ -67,26 +68,21 @@ func (i *input) benchmark(fn func() error) {
 }
 
 type output struct {
-	region   string
-	duration time.Duration
-	errors   int
+	region    string
+	durations []time.Duration
+	errors    int
+
+	med time.Duration // median of durations; calculated on first call to median()
 }
 
-type outputSorter struct {
-	averages []output
-}
+func (o *output) median() time.Duration {
+	if o.med == 0 {
+		// Sort durations and pick the middle one.
+		sort.Slice(o.durations, func(i, j int) bool {
+			return o.durations[i] < o.durations[j]
+		})
+		o.med = o.durations[len(o.durations)/2]
+	}
+	return o.med
 
-// Len is part of sort.Interface.
-func (s *outputSorter) Len() int {
-	return len(s.averages)
-}
-
-// Swap is part of sort.Interface.
-func (s *outputSorter) Swap(i, j int) {
-	s.averages[i], s.averages[j] = s.averages[j], s.averages[i]
-}
-
-// Less is part of sort.Interface.
-func (s *outputSorter) Less(i, j int) bool {
-	return s.averages[i].duration < s.averages[j].duration
 }

commit 5e542e1b3b6db2a54548058c1b1fbcbbba8e1456

fix build breakage caused by #13 and #14

main.go
@@ -132,9 +132,9 @@ func report() {
 	})
 
 	if top {
-		t := averages[0].region
+		t := all[0].region
 		if t == "global" {
-			t = averages[1].region
+			t = all[1].region
 		}
 		fmt.Print(t)
 		return