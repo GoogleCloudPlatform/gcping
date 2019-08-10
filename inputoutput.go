6c67535606e2c80cba87199e5ad406cf5145a934
jordo1138 <8525512+jordo1138@users.noreply.github.com>
2019-08-10 09:20:35 -0700

    add -multicloud flag, adjust endponts

--- inputoutput.go
+++ inputoutput.go
@@ -28,7 +28,7 @@ type input struct {
 
 func (i *input) HTTP() {
 	i.benchmark(func() error {
-		req, _ := http.NewRequest("GET", "http://"+i.endpoint+"/ping", nil)
+		req, _ := http.NewRequest("GET", i.endpoint, nil)
 		res, err := client.Do(req)
 		if err != nil {
 			return err
