f83abf3b84a8232ef181009bd6061aabaadc7a7b
saranshdhingra <saranshdhingra@google.com>
2021-06-20 02:34:30 +0530

    WIP: Regions shown on the map and their latency reflected on the marker color

--- cmd/ping/kodata/js/app.js
+++ cmd/ping/kodata/js/app.js
@@ -2,161 +2,157 @@
 // TODO: Add an option to contribute times and JS geolocation info to a public BigQuery dataset.
 
 
-class GcpZone{
-  constructor(region,label,endpoint,latency){
-      this.region=region;
-      this.label=label;
-      this.latency=latency;
-      this.pingUrl=endpoint+'/ping';
-      this.fetching=false;
-  }
-}
+let map,
+  zones = {},
+  fastestZone = '',
+  locations = getLocations(),
+  markers = {};
 
-const _MAX_RESULTS = 20; // Only consider most recent results.
-let _ENDPOINTS = {};
-let _REGIONS = [];
-let IDX = 0;
-let RESULTS = {};
-let zones=[];
-
-function median(arr) {
-  if (arr.length == 0) { return 0; }
-  let copy = arr.slice(0);
-  copy.sort();
-  return copy[Math.floor(copy.length/2)];
-}
-
-let GLOBAL_GOT = '';
-// document.addEventListener('nextping', function() {
-//   let r = _REGIONS[IDX];
-//   IDX = (IDX+1)%_REGIONS.length; // wrap around
-//   let url = _ENDPOINTS[r].URL + '/ping';
-
-//   let start = new Date().getTime();
-//   fetch(url).then((value) => {
-//     if (!value.ok) {
-//       console.log('fetch', value.url, value.ok, value.status);
-//     }
-//     value.text().then((resp) => {
-//       if (r == 'global') {
-//         console.log('global got', resp);
-//         GLOBAL_GOT = resp.trim();
-//       }
-//     });
-//     if (value.headers['X-First-Request'] == 'true') {
-//       console.log('Discarding first request for', url);
-//     } else {
-//       let took = new Date().getTime()-start;
-//       RESULTS[r].push(took);
-//       if (RESULTS[r].length > _MAX_RESULTS) {
-//         RESULTS[r].shift();
-//       }
-//       let a = median(RESULTS[r]);
-//       let out = document.getElementById(r+'-result');
-//       updateTable();
-//     }
-//     if (!stopped) {
-//       document.dispatchEvent(new Event('nextping'));
-//     }
-//   });
-// });
-
-function updateTable() {
-  let medians = [];
-  for (k in RESULTS) {
-    medians.push([k, median(RESULTS[k])]);
-  }
-  medians.sort(function(a, b) {
-    if (a[1] < b[1]) { return -1; }
-    if (a[1] > b[1]) { return 1; }
-    return 0;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 17.2667283, lng: 75.0585942 },
+    zoom: 3.5,
   });
 
-  let html = '';
-  let tweet = 'My lowest-latency #GCP regions via gcping.com:';
-  let place = 0;
-  let top = 0;
-  for (var i = 0; i < medians.length; i++) {
-    let region = medians[i][0];
-    let latency = medians[i][1];
-    if (latency == 0) { continue; }
-    if (i == 0 && region === 'global') { top++; }
-    if (region != 'global') { place++; }
-    let regionsub = region;
-    if (region == 'global' && RESULTS[GLOBAL_GOT]) { regionsub = '<i>→'+GLOBAL_GOT+'</i>'; }
-    var cls = (i == top) ? 'top' : '';
-    var desc = (_ENDPOINTS[region] || {}).RegionName || '';
-    html += '<tr class="'+cls+'"><td class="regiondesc">'+desc+'<div class="region">'+regionsub+'</div></td>' +
-      '<td class="result" id="'+region+'"><div>'+latency+' ms</div></td></tr>';
-
-    if (place <= 3 && region != 'global') {
-      tweet += '\n'+region+' ('+latency+' ms)';
-    }
-  }
-  document.getElementsByTagName('tbody')[0].innerHTML = html;
-  document.getElementById('tweet-link').href = 'https://twitter.com/share?text='+encodeURIComponent(tweet);
+  fetchZones();
 }
 
-let stopped = true;
-let ss = document.getElementById('stopstart');
-let st;
-// ss.onclick = function() {
-//   if (stopped) {
-//     console.log('starting');
-//     stopped = false;
-//     ss.children[0].innerText = 'stop';
-//     document.dispatchEvent(new Event('nextping'));
-//     st = setTimeout(ss.onclick, 30000); // stop after 30s.
-//   } else {
-//     console.log('stopping');
-//     stopped = true;
-//     clearTimeout(st); // cancel the stop timeout, as it is stopped.
-//     ss.children[0].innerText = 'play_arrow';
-//   }
-// };
+function fetchZones() {
+  fetch("/endpoints").then((resp) => {
+    return resp.json();
+  }).then(async (endpoints) => {
+    for (zone of Object.values(endpoints)) {
+      let gcpZone = { region: zone.Region, label: zone.RegionName, pingUrl: zone.URL };
+      zones[gcpZone.region] = gcpZone;
 
+      // if(fastestZone==='' || zones[fastestZone].latency>gcpZone.latency)
+      //   fastestZone=gcpZone.region;
 
-function zoneList(){
-  return {
-    zones: {},
-    fastestZone:'',
-    fetchZones:function(){
-      fetch("/endpoints").then((resp)=>{ 
-          return resp.json();
-        }).then(async (endpoints)=>{ 
-          for(zone of Object.values(endpoints)){
-              let gcpZone=new GcpZone(zone.Region,zone.RegionName,zone.URL);
-              this.zones[gcpZone.region]=gcpZone;
-              // if(gcpZone.region==='asia-south1')
-                await this.fetchZoneLatency(gcpZone.region);
-          }
-        });
-    },
-    fetchZoneLatency:function(region){
-      return new Promise((resolve)=>{
-        const gcpZone=this.zones[region],
-        start=new Date().getTime();
-
-        fetch(gcpZone.pingUrl).then((resp)=>{
-          const latency=new Date().getTime()-start;
-          gcpZone.latency=latency;
-
-          if(this.fastestZone==='' || this.zones[this.fastestZone].latency>latency)
-            this.fastestZone=region;
-
-          resolve();
-        });
-      });
-    },
-    getZoneClass:function(zone){
-      if(zone.region===this.fastestZone)
-        return {'fastest':true};
-      else if(zone.latency<=100)
-        return {'fast':true};
-      else if(zone.latency>100 && zone.latency<300)
-        return {'slow':true};
-      else
-        return {'very-slow':true};
+      await updateRegionOnMap(gcpZone.region);
+      // if(gcpZone.region==='asia-south1')
+      await fetchZoneLatency(gcpZone.region);
     }
+  });
+}
+
+function fetchZoneLatency(region) {
+  return new Promise((resolve) => {
+    const gcpZone = zones[region],
+      start = new Date().getTime();
+
+    fetch(gcpZone.pingUrl,{
+      mode: 'no-cors',
+      cache: 'no-cache'
+    }).then((resp) => {
+      const latency = new Date().getTime() - start;
+      zones[region].latency = latency;
+
+      updateRegionOnMap(region);
+
+      resolve();
+    });
+  });
+}
+
+async function updateRegionOnMap(region) {
+
+  // if there is a marker present remove it
+  if(markers[region]!==undefined){
+    markers[region].setMap=null;
+    markers[region]=null;
+  }
+
+  const image=getMarkerImage(region),
+    title=zones[region].latency === undefined ? zones[region].label : `${zones[region].label} ${zones[region].latency}ms`;
+
+  //we have the location cached
+  if (locations[region] !== undefined) {
+    const marker = new google.maps.Marker({
+      position: locations[region],
+      map: map,
+      title:title,
+      icon:image
+    });
+
+    markers[region] = marker;
+  }
+  //we fetch from the places API
+  else {
+    const location = await getLocationFromPlace(zones[region].label);
+    if (location) {
+      const marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        title:title,
+        icon:image
+      });
+
+      markers[region] = marker;
+    }
+  }
+}
+
+function getLocations() {
+  return {
+    "asia-east1": {lat: 23.4817418, lng: 118.9632941},
+    "asia-east2": {lat: 22.3526632, lng: 113.9876185},
+    "asia-northeast1": {lat: 35.5079447, lng: 139.2094288},
+    "asia-northeast2": {lat: 34.66229, lng: 135.4807797},
+    "asia-northeast3": {lat: 37.5638354, lng: 126.9040472},
+    "asia-south1": { lat: 19.0822375, lng: 72.8111468 },
+    "asia-southeast1": { lat: 1.3139991, lng: 103.7742106 },
+    "asia-southeast2": { lat: -6.2297419, lng: 106.7594786 },
+    "australia-southeast1": { lat: -33.8481647, lng: 150.7918939 },
+    "europe-north1": { lat: 64.8255751, lng: 21.5433516 },
+    "europe-west1": { lat: 50.499734, lng: 3.9057533 },
+    "europe-west2": { lat: 51.5285582, lng: -0.2416781 },
+    "europe-west3": { lat: 50.1211909, lng: 8.566525 },
+    "europe-west4": { lat: 52.2076832, lng: 4.1585844 },
+    "europe-west6": { lat: 47.3774497, lng: 8.5016958 },
+    "northamerica-northeast1": { lat: 45.5016889, lng: -73.567256 },
+    "southamerica-east1": { lat: -23.5505199, lng: -46.63330939999999 },
+    "us-central1": { lat: 41.8780025, lng: -93.097702 },
+    "us-east1": { lat: 33.836081, lng: -81.1637245 },
+    "us-east4": { lat: 37.4315734, lng: -78.6568942 },
+    "us-west1": { lat: 43.8041334, lng: -120.5542012 },
+    "us-west2": { lat: 34.0522342, lng: -118.2436849 },
+    "us-west3": { lat: 40.7607793, lng: -111.8910474 },
+    "us-west4": { lat: 36.1699412, lng: -115.1398296 }
   };
+}
+
+function getLocationFromPlace(placeLabel) {
+  return new Promise((resolve) => {
+    var request = {
+      query: placeLabel,
+      fields: ['name', 'geometry'],
+    };
+
+    let service = new google.maps.places.PlacesService(map);
+
+    service.findPlaceFromQuery(request, function (results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        resolve({ lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng() });
+      }
+
+      resolve(false);
+    });
+  });
+}
+
+function getMarkerImage(region){
+  const latency=zones[region].latency;
+
+  if(latency===undefined){
+    return "/images/marker.svg";
+  }
+  else if(latency<=100){
+    return "/images/marker-green.svg";
+  }
+  else if(latency>100 && latency<300){
+    return "/images/marker-orange.svg";
+  }
+  else{
+    return "/images/marker-red.svg";
+  }
 }
