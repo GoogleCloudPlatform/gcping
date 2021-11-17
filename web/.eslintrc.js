ebf49da64dadf9c01023a458cc58ea2dde0713a6
Chris Wilcox <crwilcox@google.com>
2021-11-17 13:26:59 -0800

    explicitly run prettier for css, html

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,31 +1,23 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true,
-    },
-    'extends': [
-        'google',
-        "plugin:prettier/recommended",
-    ],
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["google", "plugin:prettier/recommended"],
+  parserOptions: {},
+  plugins: [],
+  rules: {
+    "require-jsdoc": [
+      "error",
+      {
+        require: {
+          FunctionDeclaration: true,
+          MethodDefinition: false,
+          ClassDeclaration: false,
+          ArrowFunctionExpression: false,
+          FunctionExpression: false,
         },
-        'ecmaVersion': 13,
-        'sourceType': 'module',
-    },
-    'plugins': [
-        // 'react',
+      },
     ],
-    'rules': {
-        "require-jsdoc": ["error", {
-            "require": {
-                "FunctionDeclaration": true,
-                "MethodDefinition": false,
-                "ClassDeclaration": false,
-                "ArrowFunctionExpression": false,
-                "FunctionExpression": false
-            }
-        }]
-    },
+  },
 };
