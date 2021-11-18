fa35fb54132f48e36e8f4a542fab8aa4465feb47
Christopher Wilcox <crwilcox@google.com>
2021-11-18 09:46:25 -0800

    feat: add eslint, prettier, and npm scripts (#75)
    * chore: add prettier, eslint
    * Add changes to integration eslint prettier
    * fix: strict pins, less non-default rules
    * feat: add lint CI job
    
    Co-authored-by: Saransh Dhingra <saranshdhingra@google.com>

---
+++ web/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["google", "plugin:prettier/recommended"],
+  parserOptions: {},
+  plugins: [],
+  rules: {},
+};
