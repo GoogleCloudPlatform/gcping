7d9098817d23be12107b0c4988dee78b616dc647
Eno Compton <enocom@google.com>
2021-11-19 10:14:11 -0700

    Sort host names

--- bin/prefer-ipv4.sh
+++ bin/prefer-ipv4.sh
@@ -7,40 +7,40 @@
 # https://github.com/hashicorp/terraform-provider-google/issues/6782#issuecomment-874574409
 
 export APIS="googleapis.com
-www.googleapis.com
-storage.googleapis.com
-iam.googleapis.com
-container.googleapis.com
 cloudresourcemanager.googleapis.com
-run.googleapis.com
+container.googleapis.com
 iam.googleapis.com
-asia-east2-run.googleapis.com
-southamerica-east1.googleapis.com
-asia-south1-run.googleapis.com
-europe-west2-run.googleapis.com
-australia-southeast2-run.googleapis.com
-europe-north1-run.googleapis.com
-europe-west3-run.googleapis.com
-southamerica-east1-run.googleapis.com
-asia-southeast1-run.googleapis.com
-asia-northeast1-run.googleapis.com
-us-west3-run.googleapis.com
-us-central1-run.googleapis.com
-asia-northest2-run.googleapis.com
-us-east4-run.googleapis.com
-australia-southeast1-run.googleapis.com
-us-east4-run.googleapis.com
-europe-west3-run.googleapis.com
-asia-southeast2-run.googleapis.com
-europe-west4-run.googleapis.com
-asia-northeast2-run.googleapis.com
+iam.googleapis.com
+run.googleapis.com
+storage.googleapis.com
+www.googleapis.com
 asia-east1-run.googleapis.com
+asia-east2-run.googleapis.com
+asia-northeast1-run.googleapis.com
+asia-northeast2-run.googleapis.com
 asia-northeast3-run.googleapis.com
-us-west4-run.googleapis.com
+asia-northest2-run.googleapis.com
+asia-south1-run.googleapis.com
+asia-southeast1-run.googleapis.com
+asia-southeast2-run.googleapis.com
+australia-southeast1-run.googleapis.com
+australia-southeast2-run.googleapis.com
 europe-central2-run.googleapis.com
+europe-north1-run.googleapis.com
 europe-west1-run.googleapis.com
+europe-west2-run.googleapis.com
+europe-west3-run.googleapis.com
+europe-west3-run.googleapis.com
+europe-west4-run.googleapis.com
+northamerica-northeast1-run.googleapis.com
+southamerica-east1-run.googleapis.com
+southamerica-east1.googleapis.com
+us-central1-run.googleapis.com
+us-east4-run.googleapis.com
+us-east4-run.googleapis.com
 us-west2-run.googleapis.com
-northamerica-northeast1-run.googleapis.com"
+us-west3-run.googleapis.com
+us-west4-run.googleapis.com"
 
 for name in $APIS; do
     echo Configuring IPv4 for $name...
